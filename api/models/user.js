5bdbd5fb0b0e5de0ad11e44162b78729d96e309b
UmaAshaMS <umaasha69@gmail.com>
2025-04-03 18:17:55 +0530

    Created User Model

---
+++ api/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type:String
+    },
+    email : {
+        type: String,
+        unique:true,
+        required :true,
+    },
+    password : {
+        type:String,
+        required : true,
+    },
+    status : {
+        type : boolean
+    },
+
+
+},{timestamps: true});
+
+const User = mongoose.model('User', userSchema)
+export default User;
