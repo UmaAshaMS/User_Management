851c22e3e8a014a1506b9b725bb9717558a53bdb
UmaAshaMS <umaasha69@gmail.com>
2025-04-03 13:32:37 +0530

    Router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter ,Routes, Route} from "react-router-dom"
 import React from 'react'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <BrowserRouter>
+      <Routes>
+       
+        <Route path = '/' element ={<Home />} />
+        <Route path = '/about' element ={<About />} />
+        <Route path = '/signIn' element ={<SignIn />} />
+        <Route path = '/signUp' element ={<SignUp />} />
+        <Route path = '/profile' element ={<Profile />} />
+
+      </Routes>
+       
+      </BrowserRouter>
+      
     </div>
   )
 }
