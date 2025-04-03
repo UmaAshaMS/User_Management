3cc33e0cf6594865f415df0c1ee03dec724278c8
UmaAshaMS <umaasha69@gmail.com>
2025-04-03 18:09:16 +0530

    MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then( () => {
+        console.log('MongoDB connected')
+    })
+    .catch( (err) => {
+        console.log(err)
+    });
 
 const app = express();
 
